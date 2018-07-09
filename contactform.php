bc2471c2ac4844ec9478dfc2ad9b9b0428e8a154
techaggresso <40353579+techagresso@users.noreply.github.com>
2018-07-09 15:19:25 +0530

    First

---
+++ contactform.php
@@ -0,0 +1,18 @@
+ <?php
+/*
+if(isset($_POST["submit"])) {
+  $name=$_POST["name"];
+  $subject=$_POST["subject"];
+  $mailfrom=$_POST["email"];
+  $message=$_POST["message"];
+
+$mailTO = "mehramanish@live.com";
+$headers = "From: ".$mailfrom;
+$txt = "You have recieved an email from ".$name.".\n\n".$message;
+
+
+  mail($mailTO, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
+}
+
+*/
